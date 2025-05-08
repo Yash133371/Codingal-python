cede7bcd7b476597c4a135d74375bb490c1f470f
Yash133371 <aakashdeepalokritu@gmail.com>
2025-05-08 16:51:36 +0530

    submit

---
+++ Module 1/Activity1.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+
+print("My name is Yash Raj")
+
+print("I like playing games")
+
+print("The value of 10X2 is", 10 * 2)
---
+++ Module 1/Actvity4.py
@@ -0,0 +1,13 @@
+import keyword
+
+print("Welcome to the world of programming")
+
+# Print a number
+print(4)
+
+# Next line after print
+print("\nHello python")
+
+print("Keywords in pythons are")
+keywords = keyword.kwlist
+print(keywords)
