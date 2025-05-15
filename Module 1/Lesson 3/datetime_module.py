a2a8f7f24cdfb08d21174aaef5b59da8ee68c338
Yash133371 <aakashdeepalokritu@gmail.com>
2025-05-15 16:49:37 +0530

    submit

---
+++ Module 1/Lesson 3/datetime_module.py
@@ -0,0 +1,8 @@
+import datetime
+import calendar
+
+current_time = datetime.datetime.now()
+print("The current time is", current_time)
+
+print("Here is the calender of 2021")
+print(calendar.calendar(2021))
