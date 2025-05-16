1055e5b1a874640624fa88b2f324e6126b7a71d0
Yash133371 <aakashdeepalokritu@gmail.com>
2025-05-16 16:56:02 +0530

    submit

---
+++ Module 1/Lesson 4/sum_of_natural_numbers.py
@@ -0,0 +1,2 @@
+print(f"The sum of first 50 natural numbers is {sum(range(1, 51))}")
+print(f"The sum of first 100 natural numbers is {sum(range(1, 101))}")
---
+++ Module 1/Lesson 4/table_of_23.py
@@ -0,0 +1,3 @@
+print("The table of 23")
+for i in range(1, 11):
+    print(f"23 x {i} = {23 * i}")
