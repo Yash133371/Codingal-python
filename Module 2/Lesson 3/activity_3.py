bce2f76cb64402cdefe0692093c142c45d9b58fd
Yash133371 <aakashdeepalokritu@gmail.com>
2025-06-06 16:30:38 +0530

    submit

---
+++ Module 2/Lesson 3/activity_3.py
@@ -0,0 +1,16 @@
+class Parrot:
+
+    species = "bird"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+blu = Parrot("Blu", 16)
+woo = Parrot("Woo", 15)
+
+print(f"Blu is a {blu.species}")
+print(f"Woo is also a {woo.species}")
+
+print(f"{blu.name} is {blu.age} years old")
+print(f"{woo.name} is {woo.age} years old")
